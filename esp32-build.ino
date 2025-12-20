83304b642ad57873861badd81ca0fdf972c19660
amiru20060110-bit <amiru20060110@gmail.com>
2025-12-20 21:52:34 +0530

    Update esp32-build.ino

--- esp32-build.ino
+++ esp32-build.ino
@@ -1,30 +1,21 @@
-#include "driver/i2s.h"
-#include "esp_system.h"
+#include <driver/i2s.h>
 
-#define I2S_BCLK   35
-#define I2S_LRC    34
-#define I2S_DOUT   33
-
-#define SAMPLE_RATE 44100
-#define TONE_FREQ   1000
+#define I2S_BCLK 1
+#define I2S_LRC  2
+#define I2S_DOUT 3
 
 void setup() {
-  Serial.begin(115200);
-  delay(1000);
-  Serial.println("Starting I2S tone test");
-
   i2s_config_t i2s_config = {
     .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-    .sample_rate = SAMPLE_RATE,
+    .sample_rate = 44100,
     .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
     .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
     .communication_format = I2S_COMM_FORMAT_I2S,
-    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+    .intr_alloc_flags = 0,
     .dma_buf_count = 8,
     .dma_buf_len = 64,
     .use_apll = false,
-    .tx_desc_auto_clear = true,
-    .fixed_mclk = 0
+    .tx_desc_auto_clear = true
   };
 
   i2s_pin_config_t pin_config = {
@@ -39,13 +30,14 @@ void setup() {
 }
 
 void loop() {
-  static float phase = 0.0;
-  int16_t sample;
+  static int16_t buffer[64];
+  static int phase = 0;
+
+  for (int i = 0; i < 64; i++) {
+    buffer[i] = (phase < 32) ? 16000 : -16000;  // square wave
+    phase = (phase + 1) % 64;
+  }
+
   size_t bytes_written;
-
-  sample = (int16_t)(sin(phase) * 3000); // volume
-  phase += 2.0 * PI * TONE_FREQ / SAMPLE_RATE;
-  if (phase > 2.0 * PI) phase -= 2.0 * PI;
-
-  i2s_write(I2S_NUM_0, &sample, sizeof(sample), &bytes_written, portMAX_DELAY);
+  i2s_write(I2S_NUM_0, buffer, sizeof(buffer), &bytes_written, portMAX_DELAY);
 }
