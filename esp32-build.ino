7b10fb33e08d8ab63dcbd2f3a409870c47ba651d
amiru20060110-bit <amiru20060110@gmail.com>
2025-12-23 22:53:00 +0530

    Update esp32-build.ino

--- esp32-build.ino
+++ esp32-build.ino
@@ -15,16 +15,16 @@
 #define I2S_LRC  2
 #define I2S_DOUT 3
 
-// Matrix Config (6 Columns x 5 Rows)
-const int colPins[6] = {40, 41, 42, 33, 34, 38}; 
-const int rowPins[5] = {15, 16, 17, 18, 47}; 
+// Matrix Config (5 Columns x 5 Rows = 25 keys)
+const int colPins[5] = {40, 41, 42, 33, 34}; 
+const int rowPins[5] = {15, 16, 17, 18, 38}; 
 
-const char* soundFiles[5][6] = {
-  {"/16.wav", "/17.wav",  "/18.wav", "/19.wav", "/20.wav", "/54.wav"},
-  {"/21.wav", "/ce4.wav", "/41.wav", "/42.wav", "/43.wav", "/55.wav"},
-  {"/44.wav", "/45.wav",  "/46.wav", "/47.wav", "/48.wav", "/56.wav"},
-  {"/49.wav", "/50.wav",  "/51.wav", "/52.wav", "/53.wav", "/57.wav"},
-  {"/58.wav", "/59.wav",  "/60.wav", "/61.wav", "/62.wav", "/63.wav"}
+const char* soundFiles[5][5] = {
+  {"/16.wav", "/17.wav", "/18.wav", "/19.wav", "/20.wav"},
+  {"/21.wav", "/ce4.wav", "/41.wav", "/42.wav", "/43.wav"},
+  {"/44.wav", "/45.wav", "/46.wav", "/47.wav", "/48.wav"},
+  {"/49.wav", "/50.wav", "/51.wav", "/52.wav", "/53.wav"},
+  {"/54.wav", "/55.wav", "/56.wav", "/57.wav", "/58.wav"}
 };
 
 // Polyphony & Audio Config
@@ -41,7 +41,7 @@ struct Voice {
 };
 
 Voice voices[MAX_VOICES];
-bool keyStates[5][6] = {false}; 
+bool keyStates[5][5] = {false}; 
 
 void setupI2S() {
   i2s_config_t i2s_config = {
@@ -79,7 +79,7 @@ void setup() {
   delay(2000); // Increased delay for system/PSRAM stability
 
   // Initialize Matrix Pins
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 5; i++) {
     pinMode(colPins[i], OUTPUT);
     digitalWrite(colPins[i], LOW);
   }
@@ -95,12 +95,12 @@ void setup() {
   }
 
   setupI2S();
-  Serial.println("System Online: 30-Key / 4-Voice Polyphony");
+  Serial.println("System Online: 25-Key / 4-Voice Polyphony");
 }
 
 void loop() {
   // 1. Matrix Scanning (Col -> Row)
-  for (int c = 0; c < 6; c++) {
+  for (int c = 0; c < 5; c++) {
     digitalWrite(colPins[c], HIGH);
     delayMicroseconds(30); // Stabilization delay
     
