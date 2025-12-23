4816bec2c170e28494d38ac2b8e9cd5df7e7e93b
amiru20060110-bit <amiru20060110@gmail.com>
2025-12-23 22:18:17 +0530

    Update esp32-build.ino

--- esp32-build.ino
+++ esp32-build.ino
@@ -4,36 +4,33 @@
 #include "SPI.h"
 #include "driver/i2s.h"
 
-// --- Hardware Pins ---
+// SD Card Pins (Waveshare S3-ETH Default)
 #define SD_MOSI 6
 #define SD_MISO 5
 #define SD_CLK  7
 #define SD_CS   4
 
+// I2S Pins for MAX98357A
 #define I2S_BCLK 1
 #define I2S_LRC  2
 #define I2S_DOUT 3
 
-// Matrix Config: 6 Columns x 5 Rows
-// Swapped 48 for 38 to avoid the onboard RGB LED
-const int colPins[6] = {33, 34, 40, 41, 42, 38}; 
-const int rowPins[5] = {15, 16, 17, 18, 47}; 
+// Matrix Config (5 Columns x 4 Rows)
+const int colPins[5] = {40, 41, 42, 33, 34}; 
+const int rowPins[4] = {15, 16, 17, 18}; 
 
-const char* soundFiles[5][6] = {
-  {"/25.wav", "/26.wav", "/27.wav", "/28.wav", "/29.wav", "/30.wav"},
-  {"/31.wav", "/32.wav", "/33.wav", "/34.wav", "/35.wav", "/36.wav"},
-  {"/37.wav", "/38.wav", "/39.wav", "/40.wav", "/17.wav", "/18.wav"},
-  {"/19.wav", "/20.wav", "/21.wav", "/22.wav", "/23.wav", "/24.wav"},
-  {"/41.wav", "/42.wav", "/43.wav", "/44.wav", "/45.wav", "/46.wav"}
+const char* soundFiles[4][5] = {
+  {"/16.wav", "/17.wav",  "/18.wav", "/19.wav", "/20.wav"},
+  {"/21.wav", "/ce4.wav", "/41.wav", "/42.wav", "/43.wav"},
+  {"/44.wav", "/45.wav",  "/46.wav", "/47.wav", "/48.wav"},
+  {"/49.wav", "/50.wav",  "/51.wav", "/52.wav", "/53.wav"}
 };
 
-// --- Pro-Audio Low Latency Settings ---
+// Polyphony & Audio Config
 #define MAX_VOICES 4
 #define SAMPLE_RATE 32000
 #define I2S_NUM I2S_NUM_0
-#define BUF_SIZE 256        // Tiny buffer for instant response
-#define DMA_COUNT 4         // Minimal buffering
-#define SCAN_DELAY_US 10    // Fast matrix settling time
+#define BUF_SIZE 512 
 
 struct Voice {
   File file;
@@ -43,7 +40,7 @@ struct Voice {
 };
 
 Voice voices[MAX_VOICES];
-bool keyStates[5][6] = {false}; 
+bool keyStates[4][5] = {false}; 
 
 void setupI2S() {
   i2s_config_t i2s_config = {
@@ -53,10 +50,9 @@ void setupI2S() {
     .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
     .communication_format = I2S_COMM_FORMAT_STAND_I2S,
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = DMA_COUNT,
+    .dma_buf_count = 8,
     .dma_buf_len = BUF_SIZE,
-    .use_apll = false,
-    .tx_desc_auto_clear = true 
+    .use_apll = false
   };
   i2s_pin_config_t pin_config = {
     .bck_io_num = I2S_BCLK,
@@ -78,50 +74,56 @@ void stopVoice(int i) {
 }
 
 void setup() {
-  // Serial disabled for speed, enable only for debugging
-  // Serial.begin(115200);
+  Serial.begin(115200);
+  delay(1000); 
 
-  for (int i = 0; i < 6; i++) {
+  // Initialize Matrix Pins
+  for (int i = 0; i < 5; i++) {
     pinMode(colPins[i], OUTPUT);
     digitalWrite(colPins[i], LOW);
   }
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 4; i++) {
     pinMode(rowPins[i], INPUT_PULLDOWN);
   }
 
-  // SPI overclocked to 40MHz to handle multi-voice reading
+  // SPI Setup (20MHz for 4-voice streaming)
   SPI.begin(SD_CLK, SD_MISO, SD_MOSI, SD_CS);
-  if (!SD.begin(SD_CS, SPI, 40000000)) { 
+  if (!SD.begin(SD_CS, SPI, 20000000)) { 
+    Serial.println("SD Error!"); 
     while(1); 
   }
 
   setupI2S();
+  Serial.println("System Online: 20-Key / 4-Voice Polyphony");
 }
 
 void loop() {
-  // 1. Matrix Scan
-  for (int c = 0; c < 6; c++) {
+  // 1. Matrix Scanning (Col -> Row)
+  for (int c = 0; c < 5; c++) {
     digitalWrite(colPins[c], HIGH);
-    delayMicroseconds(SCAN_DELAY_US); 
+    delayMicroseconds(30); // Stabilization delay
     
-    for (int r = 0; r < 5; r++) {
+    for (int r = 0; r < 4; r++) {
       bool pressed = (digitalRead(rowPins[r]) == HIGH);
       
       if (pressed && !keyStates[r][c]) {
+        // Find empty voice slot for NEW press
         for (int i = 0; i < MAX_VOICES; i++) {
           if (!voices[i].active) {
             voices[i].file = SD.open(soundFiles[r][c]);
             if (voices[i].file) {
-              voices[i].file.seek(44); // Jump straight to audio data
+              voices[i].file.seek(44); // Skip header
               voices[i].active = true;
               voices[i].row = r;
               voices[i].col = c;
+              Serial.printf("Playing %s\n", soundFiles[r][c]);
             }
             break; 
           }
         }
       } 
       else if (!pressed && keyStates[r][c]) {
+        // Stop specific voice on RELEASE
         for (int i = 0; i < MAX_VOICES; i++) {
           if (voices[i].active && voices[i].row == r && voices[i].col == c) {
             stopVoice(i);
@@ -133,7 +135,7 @@ void loop() {
     digitalWrite(colPins[c], LOW);
   }
 
-  // 2. High-Speed Audio Mixing
+  // 2. Mixing Engine
   int16_t mixBuf[BUF_SIZE];
   memset(mixBuf, 0, sizeof(mixBuf));
   bool anyActive = false;
@@ -147,8 +149,8 @@ void loop() {
         int samplesRead = bytesRead / 2;
         
         for (int j = 0; j < samplesRead; j++) {
+          // Summing with 32-bit math and hard-clamping
           int32_t mixed = (int32_t)mixBuf[j] + (int32_t)tempBuf[j];
-          // Optimization: Hard Clipping
           if (mixed > 32767) mixed = 32767;
           else if (mixed < -32768) mixed = -32768;
           mixBuf[j] = (int16_t)mixed;
@@ -159,9 +161,10 @@ void loop() {
     }
   }
 
-  // 3. I2S Write (Zero timeout = No waiting)
   if (anyActive) {
     size_t written;
-    i2s_write(I2S_NUM, mixBuf, sizeof(mixBuf), &written, 0);
+    i2s_write(I2S_NUM, mixBuf, sizeof(mixBuf), &written, portMAX_DELAY);
+  } else {
+    i2s_zero_dma_buffer(I2S_NUM);
   }
 }
