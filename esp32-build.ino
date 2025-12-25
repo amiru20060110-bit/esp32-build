50262cacabc39b4f6da67ba5fe46b6b39896f5f9
amiru20060110-bit <amiru20060110@gmail.com>
2025-12-25 19:31:28 +0530

    Update esp32-build.ino

--- esp32-build.ino
+++ esp32-build.ino
@@ -4,7 +4,6 @@
 #include "SPI.h"
 #include "driver/i2s.h"
 
-// Hardware Pin Definitions
 #define SD_MOSI 6
 #define SD_MISO 5
 #define SD_CLK  7
@@ -13,7 +12,6 @@
 #define I2S_LRC  44
 #define I2S_DOUT 3
 
-// Matrix Config
 const int colPins[8] = {40, 41, 42, 33, 34, 35, 39, 2}; 
 const int rowPins[8] = {15, 16, 17, 18, 38, 36, 37, 45}; 
 
@@ -25,29 +23,29 @@ const char* soundFiles[8][8] = {
   {"/48.wav", "/49.wav", "/50.wav", "/51.wav", "/52.wav", "/53.wav", "/54.wav", "/55.wav"},
   {"/56.wav", "/57.wav", "/58.wav", "/59.wav", "/60.wav", "/61.wav", "/62.wav", "/63.wav"},
   {"/64.wav", "/65.wav", "/66.wav", "/67.wav", "/68.wav", "/69.wav", "/70.wav", "/71.wav"},
-  {"/72.wav", "/73.wav", "/74.wav", "/75.wav", "/76.wav", "none", "none", "none"}
+  {"/72.wav", "/73.wav", "/74.wav", "/75.wav", "/76.wav", "SWITCH", "none", "none"}
 };
 
 #define MAX_VOICES 4
 #define SAMPLE_RATE 32000
 #define BUF_SIZE 256 
-#define FADE_SAMPLES 120 // ~3.7ms fade-in to prevent the "pop" at start
+#define FADE_SAMPLES 120 
 
 struct Voice {
   File file;
-  int row = -1;
-  int col = -1;
+  int row = -1, col = -1;
   bool active = false;
-  uint32_t samplesPlayed = 0; // Tracks progress for the soft-start fade
+  uint32_t samplesPlayed = 0;
 };
 
 Voice voices[MAX_VOICES];
 volatile bool sharedKeys[8][8] = {false}; 
 bool keyStates[8][8] = {false}; 
+int currentBank = 0; // 0 = Piano (P), 1 = Xylophone (X)
 
 void setupI2S() {
   i2s_config_t i2s_config = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX), // Fixed for Core 3.3.5
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
     .sample_rate = SAMPLE_RATE,
     .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
     .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
@@ -64,7 +62,6 @@ void setupI2S() {
   i2s_set_pin(I2S_NUM_0, &pin_config);
 }
 
-// Core 0 Task: Matrix Scanning
 void scanTask(void * pvParameters) {
   for(;;) {
     for (int c = 0; c < 8; c++) {
@@ -79,53 +76,49 @@ void scanTask(void * pvParameters) {
   }
 }
 
-void stopVoice(int i) {
-  if (voices[i].active) {
-    voices[i].file.close();
-    voices[i].active = false;
-    voices[i].samplesPlayed = 0;
-    voices[i].row = -1;
-    voices[i].col = -1;
-  }
-}
-
 void setup() {
   for (int i = 0; i < 8; i++) {
     pinMode(colPins[i], OUTPUT); digitalWrite(colPins[i], LOW);
     pinMode(rowPins[i], INPUT_PULLDOWN);
   }
-
   SPI.begin(SD_CLK, SD_MISO, SD_MOSI, SD_CS);
-  if (!SD.begin(SD_CS, SPI, 40000000)) { while(1); } // 40MHz for multi-note speed
-
+  SD.begin(SD_CS, SPI, 40000000);
   setupI2S();
-  xTaskCreatePinnedToCore(scanTask, "Scanner", 4096, NULL, 1, NULL, 0); // Core 0
+  xTaskCreatePinnedToCore(scanTask, "Scanner", 4096, NULL, 1, NULL, 0);
 }
 
 void loop() {
-  // Core 1: Handle Logic and Audio
   for (int c = 0; c < 8; c++) {
     for (int r = 0; r < 8; r++) {
       bool pressed = sharedKeys[r][c];
       
       if (pressed && !keyStates[r][c]) {
-        for (int i = 0; i < MAX_VOICES; i++) {
-          if (!voices[i].active) {
-            voices[i].file = SD.open(soundFiles[r][c]);
-            if (voices[i].file) {
-              voices[i].file.seek(44); 
-              voices[i].active = true;
-              voices[i].samplesPlayed = 0; // Prepare for fade-in
-              voices[i].row = r; voices[i].col = c;
+        // Check if this is the Instrument Switch (Row 7, Col 5)
+        if (r == 7 && c == 5) {
+          currentBank = (currentBank == 0) ? 1 : 0;
+        } 
+        else if (strcmp(soundFiles[r][c], "none") != 0) {
+          for (int i = 0; i < MAX_VOICES; i++) {
+            if (!voices[i].active) {
+              char fullPath[32];
+              // Construct filename: /P16.wav or /X16.wav
+              snprintf(fullPath, sizeof(fullPath), "/%c%s", (currentBank == 0 ? 'P' : 'X'), soundFiles[r][c] + 1);
+              voices[i].file = SD.open(fullPath);
+              if (voices[i].file) {
+                voices[i].file.seek(44); 
+                voices[i].active = true;
+                voices[i].samplesPlayed = 0;
+                voices[i].row = r; voices[i].col = c;
+              }
+              break; 
             }
-            break; 
           }
         }
       } 
       else if (!pressed && keyStates[r][c]) {
         for (int i = 0; i < MAX_VOICES; i++) {
           if (voices[i].active && voices[i].row == r && voices[i].col == c) {
-            stopVoice(i);
+            voices[i].file.close(); voices[i].active = false;
           }
         }
       }
@@ -135,28 +128,19 @@ void loop() {
 
   int16_t mixBuf[BUF_SIZE] = {0};
   bool anyActive = false;
-
   for (int i = 0; i < MAX_VOICES; i++) {
     if (voices[i].active && voices[i].file.available()) {
       anyActive = true;
       int16_t tempBuf[BUF_SIZE];
       size_t bytesRead = voices[i].file.read((uint8_t*)tempBuf, BUF_SIZE * 2);
-      int samplesRead = bytesRead / 2;
-      
-      for (int j = 0; j < samplesRead; j++) {
-        // Soft-Start Logic: Smoothly ramp volume from 0 to 1
-        float vol = 1.0;
-        if (voices[i].samplesPlayed < FADE_SAMPLES) {
-          vol = (float)voices[i].samplesPlayed / FADE_SAMPLES;
-        }
-
-        int32_t sample = (int32_t)tempBuf[j] * vol;
-        int32_t mixed = (int32_t)mixBuf[j] + sample;
+      for (int j = 0; j < (bytesRead/2); j++) {
+        float vol = (voices[i].samplesPlayed < FADE_SAMPLES) ? (float)voices[i].samplesPlayed / FADE_SAMPLES : 1.0;
+        int32_t mixed = (int32_t)mixBuf[j] + (int16_t)((float)tempBuf[j] * vol);
         mixBuf[j] = (int16_t)constrain(mixed, -32768, 32767);
         voices[i].samplesPlayed++;
       }
     } else if (voices[i].active) {
-      stopVoice(i);
+      voices[i].file.close(); voices[i].active = false;
     }
   }
 
@@ -164,6 +148,6 @@ void loop() {
     size_t written;
     i2s_write(I2S_NUM_0, mixBuf, sizeof(mixBuf), &written, portMAX_DELAY);
   } else {
-    i2s_zero_dma_buffer(I2S_NUM_0); // Prevents buzzing/hissing
+    i2s_zero_dma_buffer(I2S_NUM_0);
   }
 }
